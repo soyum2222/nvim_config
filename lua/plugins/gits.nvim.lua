218bb5d33300ff60803c37496a6b861e1b76e6cb
silver <silver-be@gate.me>
2025-02-06 23:56:01 +0800

    commit

--- lua/plugins/gits.nvim.lua
+++ lua/plugins/gits.nvim.lua
@@ -1,9 +1,16 @@
-return
-{
+return {
 	{
 
 		"lewis6991/gitsigns.nvim",
 		opts = {
+
+			current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+				delay = 100,
+				ignore_whitespace = false,
+			},
 			signs = {
 				add = { text = "▎" },
 				change = { text = "▎" },
@@ -69,11 +76,11 @@ return
 	{
 		"FabijanZulj/blame.nvim",
 		config = function()
-			require('blame').setup {}
+			require("blame").setup({})
 		end,
 	},
 	{
 
-		'tpope/vim-fugitive',
+		"tpope/vim-fugitive",
 	},
 }
