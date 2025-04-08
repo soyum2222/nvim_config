e416e84ad412961ca80babf1e69e1dfc7e15c36a
silver <silver-be@gate.me>
2025-04-08 13:54:31 +0800

    commit

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -183,7 +183,7 @@ return {
 					vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
 
 					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "gd", builtin.lsp_definitions, opts)
 					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 					vim.keymap.set("n", "gi", builtin.lsp_implementations, opts)
 					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
