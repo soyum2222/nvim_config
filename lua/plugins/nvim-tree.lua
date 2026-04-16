caa1c92493989b0383ef5b201c4a616198179d56
soyum <soyum2222@163.com>
2026-04-16 21:28:28 +0800

    commit

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -7,7 +7,15 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
-		require("nvim-tree").setup {}
+		require("nvim-tree").setup {
+			git = {
+				ignore = false,
+			},
+			filters = {
+				git_ignored = false,
+				dotfiles = false,
+			},
+		}
 	end,
 
 }
